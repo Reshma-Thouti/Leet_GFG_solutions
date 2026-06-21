ece5e941126aebd759972fa1f8056479803da505
Reshma-Thouti <thoutireshma@gmail.com>
2026-06-21 19:02:56 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Add Two Numbers/Solution.py
+++ LeetCode/Python/Medium/Add Two Numbers/Solution.py
@@ -1,22 +1,13 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        c=0
-        cs=0
-        res=ListNode(0)
-        ans=res
-        while l1 or l2 or c:
-            cs=l1.val if l1 else 0 
-            cs+=l2.val if l2 else 0
-            ans.next=ListNode((c+cs)%10)
-            ans=ans.next
-            c=(c+cs)//10
-            if l1:
-                l1=l1.next
-            if l2:
-                l2=l2.next
-        return res.next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
