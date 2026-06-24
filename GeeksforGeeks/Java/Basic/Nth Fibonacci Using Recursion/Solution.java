64a70be2479cf13e5cfa5b1d4527bb9167bf8797
Reshma-Thouti <thoutireshma@gmail.com>
2026-06-24 10:39:46 +0530

    Add Basic Java Solution from GeeksforGeeks

--- GeeksforGeeks/Java/Basic/Nth Fibonacci Using Recursion/Solution.java
+++ GeeksforGeeks/Java/Basic/Nth Fibonacci Using Recursion/Solution.java
@@ -1,4 +1,13 @@
-int nthFibonacci(int n) {
-    // code here
-    
-}
+class Solution:
+    def nthFibonacci(self, n):
+        # Code here
+        if n==0 or n==1:
+            return n
+        p1=0
+        p2=1
+        num=p1+p2
+        def fun(n,num,p1,p2):
+            if n==0:
+                return num
+            return fun(n-1,p2+num,p2,num)
+        return fun(n-2,num,p1,p2)
