7646300dd23433fc6743d449b4890017bb9cef8f
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-14 12:19:44 +0530

    Add Medium Python Solution from LeetCode

---
+++ LeetCode/Python/Medium/Course Schedule II/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        nc=numCourses
+        c=[[] for _ in range(nc)]
+        id=[0]*nc
+        for u,v in prerequisites:
+            c[v].append(u)
+            id[u]+=1
+        q=deque()
+        for i in range(nc):
+            if id[i]==0:
+                q.append(i)
+        res=[]
+        while q:
+            node=q.popleft()
+            res.append(node)
+            for ele in c[node]:
+                id[ele]-=1
+                if id[ele]==0:
+                    q.append(ele)
+        return res
