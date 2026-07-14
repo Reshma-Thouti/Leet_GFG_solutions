3ab6e2bd385e00a1d7d3a5fa0494dbfd251b731b
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-14 12:27:11 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Course Schedule II/Solution.py
+++ LeetCode/Python/Medium/Course Schedule II/Solution.py
@@ -11,11 +11,16 @@ class Solution:
             if id[i]==0:
                 q.append(i)
         res=[]
+        mc=0
         while q:
             node=q.popleft()
+            mc=0
             res.append(node)
             for ele in c[node]:
                 id[ele]-=1
                 if id[ele]==0:
                     q.append(ele)
-        return res
+        if mc==nc:
+            return res
+        else:
+            return []
