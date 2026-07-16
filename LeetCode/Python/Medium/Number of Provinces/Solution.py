a4b37871921bfb858a025787544e072a8c6ff728
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-16 16:11:22 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Number of Provinces/Solution.py
+++ LeetCode/Python/Medium/Number of Provinces/Solution.py
@@ -1,32 +1,36 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n=len(isConnected)
-        m=len(isConnected)
-        c=n
-        p=[i for i in range(n)]
-        r=[0]*n
+        n = len(isConnected)
+        p = [i for i in range(n)]
+        r = [0] * n
+        c = n
+
         for i in range(n):
-            for j in range(m):
-                if i!=j and isConnected[i][j]==1:
-                    if self.uni(i,j,p,r):
-                        c-=1
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    if self.uni(i, j, p, r):
+                        c -= 1
+
         return c
-    def find(self,x,p):
-            if p[x]!=x:
-                return self.find(p[x],p)
-            return p[x]
-    def uni(self,x,y,p,r):
-            px=self.find(x,p)
-            py=self.find(y,p)
 
-            if px==py:
-                return False
-            if r[px]>r[py]:
-                p[py]=px
-            elif r[px]<r[py]:
-                p[px]=px
-            else:
-                p[py]=px
-                r[px]+=1
-            return True
+    def find(self, x, p):
+        if p[x] != x:
+            p[x] = self.find(p[x], p) 
+        return p[x]
 
+    def uni(self, x, y, p, r):
+        px = self.find(x, p)
+        py = self.find(y, p)
+
+        if px == py:
+            return False
+
+        if r[px] > r[py]:
+            p[py] = px
+        elif r[px] < r[py]:
+            p[px] = py
+        else:
+            p[py] = px
+            r[px] += 1
+
+        return True
