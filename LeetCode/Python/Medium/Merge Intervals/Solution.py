71ca7e049bc839ca83ff7e64e3c7d97f1b0bea62
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-18 09:33:42 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Merge Intervals/Solution.py
+++ LeetCode/Python/Medium/Merge Intervals/Solution.py
@@ -5,7 +5,8 @@ class Solution:
         res=[intervals[0]]
         for i in range(1,n):
             if res[-1][1]>=intervals[i][0]:
-                res[-1][1]=intervals[i][1]
+                if(res[-1][1]<intervals[i][1]):
+                    res[-1][1]=intervals[i][1]
             else:
                 res.append(intervals[i])
         return res
