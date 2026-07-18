d1de03b8c10f6e0d947bb2e16aae39e9e5d82562
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-18 11:58:16 +0530

    Add Easy Python Solution from LeetCode

---
+++ LeetCode/Python/Easy/Move Zeroes/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j=0
+        n=len(nums)
+        for i in range(0,n):
+            if i!=j and nums[i]!=0:
+                nums[i], nums[j]=nums[j],nums[i]
+                j+=1
+        
+                
+
+
