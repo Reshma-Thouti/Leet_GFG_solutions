ab3e98d5b102a581d784551af61f75c90889d041
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-18 11:59:46 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/Move Zeroes/Solution.py
+++ LeetCode/Python/Easy/Move Zeroes/Solution.py
@@ -3,12 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        j=0
+        j=-1
         n=len(nums)
         for i in range(0,n):
+            if j==-1 and nums[i]==0:
+                j=i
             if i!=j and nums[i]!=0:
                 nums[i], nums[j]=nums[j],nums[i]
                 j+=1
+            
         
                 
 
