573fe5c1faf53cc2ac4cb3045841387fcae03787
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-20 10:13:14 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/Is Subsequence/Solution.py
+++ LeetCode/Python/Easy/Is Subsequence/Solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        ls=len(s)
-        lt=len(t)
-        j=0
-        if ls>lt:
-            for i in range(ls):
-                if s[i]==t[j]:
-                    j+=1
-        else:
-            for i in range(lt):
-                if s[j]==t[i]:
-                    j+=1
-        return j==min(ls,lt)
+        if s=="":
+            return True
+        i=0
+        for ch1 in t:
+            if ch1==s[i]:
+                i+=1
+                if i==len(s):
+                    return True
+        return False
 
