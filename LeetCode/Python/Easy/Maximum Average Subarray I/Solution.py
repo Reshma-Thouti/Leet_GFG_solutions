56f62ebc5db9035f67645627fe1c461b833a3dcb
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-21 18:15:33 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/Maximum Average Subarray I/Solution.py
+++ LeetCode/Python/Easy/Maximum Average Subarray I/Solution.py
@@ -1,9 +1,18 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         n=len(nums)
-        s=0
-        res=0
-        for i in range(n-k+1):
-            s=sum(nums[i:i+k])
+        # if n<=1:
+        #     return float(nums[0])
+        # s=0
+        # res=0
+        # for i in range(n-k+1):
+        #     s=sum(nums[i:i+k])
+        #     res=max(res, s/k)
+        # return res
+
+        s=sum(nums[:k])
+        res=s/k
+        for i in range(k,n):
+            s=s-nums[i-k]+nums[i]
             res=max(res, s/k)
         return res
