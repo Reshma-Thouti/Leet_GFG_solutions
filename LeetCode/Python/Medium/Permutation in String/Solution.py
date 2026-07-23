05714fc5edd972b07e75a86ffed7e59dd42aef53
Reshma-Thouti <thoutireshma@gmail.com>
2026-07-23 12:32:33 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Permutation in String/Solution.py
+++ LeetCode/Python/Medium/Permutation in String/Solution.py
@@ -1,20 +1,22 @@
 class Solution:
-    def permutations(self, res, s1,i):
-            if i==len(s1):
-                res.append("".join(s1))
-                return
-            for j in range(i,len(s1)):
-                s1[i],s1[j]=s1[j],s1[i]
-                self.permutations(res, s1, i+1)
-                s1[i],s1[j]=s1[j],s1[i]
-    
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1=list(s1)
-        res=[]
-    
-        self.permutations(res,s1, 0)
+        n = len(s1)
+        m = len(s2)
 
-        for i in range(len(res)):
-            if res[i] in s2:
+        if n > m:
+            return False
+        target = Counter(s1)
+        window = Counter(s2[:n])
+        if target == window:
+            return True
+
+        for i in range(n, m):
+            window[s2[i]] += 1
+            left = s2[i-n]
+            window[left] -= 1
+            if window[left] == 0:
+                del window[left]
+            if window == target:
                 return True
+
         return False
